a104d1e93adcf5aa06f3e5664409d363e8299186
Joe Shufflebotham <joe.shufflebotham13@gmail.com>
2020-10-06 19:08:02 +0100

    update service to return an entity per bin type

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import datetime
 
 """
 The "york_bins" custom component.
@@ -14,8 +15,36 @@ DOMAIN = "york_bins"
 ATTR_ID_NAME = "property_id"
 DEFAULT_ID = "None"
 
-def setup(hass, config):
 
+def timestamp_from_api_response(resp):
+    try:
+        epoch = int(resp[6:-2]) / 1000
+        time = datetime.fromtimestamp(epoch)
+        return time.strftime('%d/%m/%Y')
+    except:
+        return "ERROR (converting time)"
+
+
+def days_until_collection(resp):
+    try:
+        epoch = int(resp[6:-2]) / 1000
+        time = datetime.fromtimestamp(epoch)
+        now = datetime.now()
+
+        diff = time - now
+        return diff.days + 1
+    except:
+        return "ERROR (calc days)"
+
+
+def get_from_json(obj, key, default="N/F"):
+    try:
+        return obj[key]
+    except:
+        return default
+
+
+def setup(hass, config):
     def handle_get(call):
         """Handle the service call."""
         property_id = call.data.get(ATTR_ID_NAME, DEFAULT_ID)
@@ -24,14 +53,50 @@ def setup(hass, config):
 
         result = requests.request('GET', endpoint)
 
+        hass.states.set(DOMAIN + ".last_request", str(datetime.now()))
+
         if result.status_code != 200:
             print("Error making get request")
+            hass.states.set(DOMAIN + ".last_result", "Web Error (" + str(result.status_code) + ")")
         else:
             json_response = json.loads(result.content)
+            bins = len(json_response)
 
-            hass.states.set("york_bins.next_date", "")
+            if bins > 0:
+                hass.states.set(DOMAIN + ".last_result", str(bins) + " bins found.")
+                hass.states.set(DOMAIN + ".short_address", json_response[0]['ShortAddress'])
+            else:
+                hass.states.set(DOMAIN + ".last_result", "No Bins Found")
+
+            for bin in json_response:
+                waste_type = str(bin['WasteType'].lower().replace(" ", "_").replace("/", "_"))
+                entity_domain = DOMAIN + "." + waste_type
+
+                collection_available = get_from_json(bin, 'CollectionAvailable')
+
+                if collection_available == "Y":
+                    collection_available = "available"
+                else:
+                    collection_available = "not_available"
+
+                hass.states.set(entity_domain, collection_available, {
+                    "desc": get_from_json(bin, 'WasteTypeDescription'),
+                    "last_collection": timestamp_from_api_response(bin['LastCollection']),
+                    "next_collection": timestamp_from_api_response(bin['NextCollection']),
+                    "collection_in_days": days_until_collection(bin['NextCollection']),
+                    "collection_day": get_from_json(bin, 'CollectionDay'),
+                    "collection_day_full": get_from_json(bin, 'CollectionDayFull'),
+                    "type": get_from_json(bin, 'BinType'),
+                    "type_desc": get_from_json(bin, 'BinTypeDescription'),
+                    "materials": get_from_json(bin, 'MaterialsCollected'),
+                    "provider": get_from_json(bin, 'Provider'),
+                    "image": get_from_json(bin, 'ImageName'),
+                    "collection_point": get_from_json(bin, 'CollectionPoint'),
+                    "collection_point_desc": get_from_json(bin, 'CollectionPointDescription'),
+                    "number_of_bins": get_from_json(bin, 'NumberOfBins')
+                })
+
 
     hass.services.register(DOMAIN, "get", handle_get)
-
     # Return boolean to indicate that initialization was successfully.
     return True
--- api_hit.py
+++ api_hit.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import datetime
 
 property_id = "100050589619"
 
@@ -8,4 +9,22 @@ endpoint = "https://doitonline.york.gov.uk/BinsApi/EXOR/getWasteCollectionDataby
 result = requests.request('GET', endpoint)
 json_response = json.loads(result.content)
 
-print(json_response)
+next_collection_date = None
+
+for collection in json_response:
+    if next_collection_date is None:
+        next_collection_date = int(collection['NextCollection'][6:-2])
+        continue
+
+    this_date = int(collection['NextCollection'][6:-2])
+
+    if this_date < next_collection_date:
+        next_collection_date = this_date
+
+
+
+for collection in json_response:
+    epoch = int(collection['NextCollection'][6:-2]) / 1000
+    print(epoch)
+    time = datetime.fromtimestamp(epoch)
+    print(time.strftime('%d/%m/%Y'))
